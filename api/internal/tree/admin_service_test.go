a2838a3ca10312ff2258ee73e44d78f25f3b4d05
Zexuan-Xie <261095646@qq.com>
2026-06-04 11:59:52 +0800

    omx(team): auto-checkpoint worker-1 [1]

---
+++ api/internal/tree/admin_service_test.go
@@ -0,0 +1,34 @@
+package tree
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/google/uuid"
+)
+
+func TestAdminCreateNodeRejectsReservedRootSlug(t *testing.T) {
+	repo := newFakeAdminRepository()
+	service := NewAdminService(repo, nil)
+
+	_, err := service.CreateNode(context.Background(), CreateNodeInput{
+		Kind: NodeKindDirectory,
+		Name: "Admin",
+		Slug: "admin",
+	})
+	if !errors.Is(err, ErrReservedRootSlug) {
+		t.Fatalf("CreateNode() error = %v, want ErrReservedRootSlug", err)
+	}
+	if len(repo.nodes) != 0 {
+		t.Fatalf("created nodes = %d, want 0", len(repo.nodes))
+	}
+}
+
+type fakeAdminRepository struct {
+	nodes map[uuid.UUID]Node
+}
+
+func newFakeAdminRepository() *fakeAdminRepository {
+	return &fakeAdminRepository{nodes: map[uuid.UUID]Node{}}
+}
